7b252970f8329fca2085c54aa4a206a72fca583c
kashyappoonam1485-debug <kashyappoonam1485@gmail.com>
2026-07-28 00:43:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,98 @@
+let cart = [];
+let total = 0;
+
+function addToCart(product, price){
+  cart.push(product);
+  total = total + price;
+
+  document.getElementById("cartCount").innerHTML = cart.length;
+
+  alert(product + " added to cart 🛒");
+}
+
+function showCart(){
+  let items = "";
+
+  if(cart.length === 0){
+    items = "Cart is empty";
+  } else {
+    for(let i = 0; i < cart.length; i++){
+      items += cart[i] + " <button onclick=\"removeItem(" + i + ")\">Remove</button><br>";
+    }
+  }
+
+  document.getElementById("cartItems").innerHTML = items;
+  document.getElementById("totalPrice").innerHTML = "Total: ₹" + total;
+  document.getElementById("quantity").innerHTML = "Quantity: " + cart.length;
+}
+
+function removeItem(index){
+  cart.splice(index, 1);
+  document.getElementById("cartCount").innerHTML = cart.length;
+  showCart();
+}
+
+function checkout(){
+  alert("Thank you for your order! 🛍️");
+}
+
+function placeOrder(){
+  alert("Order placed successfully! 🛍️");
+}
+
+function searchProduct(){
+  let input = document.getElementById("search").value.toLowerCase();
+  let products = document.getElementsByClassName("card");
+
+  for(let i = 0; i < products.length; i++){
+    let productName = products[i].innerText.toLowerCase();
+
+    if(productName.includes(input)){
+      products[i].style.display = "block";
+    } else {
+      products[i].style.display = "none";
+    }
+  }
+}function login(){
+  let name = prompt("Enter your name:");
+
+  if(name){
+    alert("Welcome " + name + " to Preeti Fashion Hub 🛍️");
+  }
+}let orders = [];
+
+function placeOrder(){
+  orders.push(cart.join(", "));
+  alert("Order placed successfully! 🛍️");
+}
+
+function showOrders(){
+  if(orders.length === 0){
+    alert("No orders yet 📦");
+  } else {
+    alert("Your Orders: " + orders.join(" | "));
+  }
+}function filterCategory(category){
+  let products = document.getElementsByClassName("card");
+
+  for(let i = 0; i < products.length; i++){
+    let name = products[i].innerText;
+
+    if(name.includes(category)){
+      products[i].style.display = "block";
+    } else {
+      products[i].style.display = "none";
+    }
+  }
+}function showAllProducts(){
+  let products = document.getElementsByClassName("card");
+
+  for(let i = 0; i < products.length; i++){
+    products[i].style.display = "block";
+  }
+}let wishlist = [];
+
+function addWishlist(product){
+  wishlist.push(product);
+  alert(product + " added to wishlist ❤️");
+}
